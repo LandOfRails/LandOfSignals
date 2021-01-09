abb89e1906df0e2c52045596f9d475844a5ea7a5
MarkenJaden <jjsch1410@gmail.com>
2021-01-09 02:07:49 +0100

    FIXED LANGUAGE FILE BECAUSE OF STUPID (sry for shitty commit comment xd)

--- src/main/java/net/landofrails/landofsignals/LOSItems.java
+++ src/main/java/net/landofrails/landofsignals/LOSItems.java
@@ -8,26 +8,26 @@ public class LOSItems {
 
     }
 
-    public static final ItemSignalSO12 ITEM_SIGNALSO12 = new ItemSignalSO12(LandOfSignals.MODID, "ItemSignalSO12");
-    public static final ItemSignalLever ITEM_SIGNAL_LEVER = new ItemSignalLever(LandOfSignals.MODID, "ItemSignalLever");
-    public static final ItemTicketMachineDB ITEM_TICKET_MACHINE_DB = new ItemTicketMachineDB(LandOfSignals.MODID, "ItemTicketMachineDB");
-    public static final ItemTicketMachineSBB ITEM_TICKET_MACHINE_SBB = new ItemTicketMachineSBB(LandOfSignals.MODID, "ItemTicketMachineSBB");
-    public static final ItemSignalBox ITEM_SIGNAL_BOX = new ItemSignalBox(LandOfSignals.MODID, "ItemSignalBox");
-    public static final ItemConnector ITEM_CONNECTOR = new ItemConnector(LandOfSignals.MODID, "ItemConnector");
+    public static final ItemSignalSO12 ITEM_SIGNALSO12 = new ItemSignalSO12(LandOfSignals.MODID, "item_signal_so12");
+    public static final ItemSignalLever ITEM_SIGNAL_LEVER = new ItemSignalLever(LandOfSignals.MODID, "item_signal_lever");
+    public static final ItemTicketMachineDB ITEM_TICKET_MACHINE_DB = new ItemTicketMachineDB(LandOfSignals.MODID, "item_ticket_machine_db");
+    public static final ItemTicketMachineSBB ITEM_TICKET_MACHINE_SBB = new ItemTicketMachineSBB(LandOfSignals.MODID, "item_ticket_machine_sbb");
+    public static final ItemSignalBox ITEM_SIGNAL_BOX = new ItemSignalBox(LandOfSignals.MODID, "item_signal_box");
+    public static final ItemConnector ITEM_CONNECTOR = new ItemConnector(LandOfSignals.MODID, "item_connector");
 
     //GroundItems
-    public static final ItemGround ITEM_GROUND_VORSIGNAL = new ItemGround(LandOfSignals.MODID, "ItemGroundVorsignal", "BLOCK_GROUND_VORSIGNAL");
-    public static final ItemGround ITEM_GROUND_GAMERTV = new ItemGround(LandOfSignals.MODID, "ItemGroundGamerTV", "BLOCK_GROUND_GAMERTV");
+    public static final ItemGround ITEM_GROUND_VORSIGNAL = new ItemGround(LandOfSignals.MODID, "item_ground_vorsignal", "BLOCK_GROUND_VORSIGNAL");
+    public static final ItemGround ITEM_GROUND_GAMERTV = new ItemGround(LandOfSignals.MODID, "item_ground_gamertv", "BLOCK_GROUND_GAMERTV");
 
     //MidItems
-    public static final ItemMid ITEM_MID_VORSIGNAL_MAST = new ItemMid(LandOfSignals.MODID, "ItemMidVorsignalMast", "BLOCK_MID_VORSIGNAL_MAST");
-    public static final ItemMid ITEM_MID_GAMERTV = new ItemMid(LandOfSignals.MODID, "ItemMidGamerTV", "BLOCK_MID_GAMERTV");
+    public static final ItemMid ITEM_MID_VORSIGNAL_MAST = new ItemMid(LandOfSignals.MODID, "item_mid_vorsignal_mast", "BLOCK_MID_VORSIGNAL_MAST");
+    public static final ItemMid ITEM_MID_GAMERTV = new ItemMid(LandOfSignals.MODID, "item_mid_gamertv", "BLOCK_MID_GAMERTV");
 
     //TopItems
-    public static final ItemTop ITEM_TOP_GAMERTV_HVHP = new ItemTop(LandOfSignals.MODID, "ItemTopGamertvHvhp", "BLOCK_TOP_GAMERTV_HVHP");
-    public static final ItemTop ITEM_TOP_VORSIGNAL_KOPF = new ItemTop(LandOfSignals.MODID, "ItemTopVorsignalKopf", "BLOCK_TOP_VORSIGNAL_KOPF");
-    public static final ItemTop ITEM_TOP_GAMERTV_VORSIGNAL = new ItemTop(LandOfSignals.MODID, "ItemTopGamertvVorsignal", "BLOCK_TOP_GAMERTV_VORSIGNAL");
-    public static final ItemTop ITEM_TOP_GAMERTV_HVERSATZ = new ItemTop(LandOfSignals.MODID, "ItemTopGamertvHversatz", "BLOCK_TOP_GAMERTV_HVERSATZ");
+    public static final ItemTop ITEM_TOP_VORSIGNAL_KOPF = new ItemTop(LandOfSignals.MODID, "item_top_vorsignal_kopf", "BLOCK_TOP_VORSIGNAL_KOPF");
+    public static final ItemTop ITEM_TOP_GAMERTV_HVHP = new ItemTop(LandOfSignals.MODID, "item_top_gamertv_hvhp", "BLOCK_TOP_GAMERTV_HVHP");
+    public static final ItemTop ITEM_TOP_GAMERTV_VORSIGNAL = new ItemTop(LandOfSignals.MODID, "item_top_gamertv_vorsignal", "BLOCK_TOP_GAMERTV_VORSIGNAL");
+    public static final ItemTop ITEM_TOP_GAMERTV_HVERSATZ = new ItemTop(LandOfSignals.MODID, "item_top_gamertv_hversatz", "BLOCK_TOP_GAMERTV_HVERSATZ");
 
     public static void register() {
         // loads static classes and ctrs
