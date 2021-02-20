a582c47984aeb34d9fc03dafcac48b521088ebb7
MarkenJaden <jjsch1410@gmail.com>
2021-02-20 23:41:54 +0100

    Added scan for same block up and down

--- src/main/java/net/landofrails/landofsignals/packet/ManipulatorToClientPacket.java
+++ src/main/java/net/landofrails/landofsignals/packet/ManipulatorToClientPacket.java
@@ -8,6 +8,9 @@ import cam72cam.mod.net.Packet;
 import cam72cam.mod.serialization.TagField;
 import net.landofrails.landofsignals.utils.IManipulate;
 
+import java.util.Collections;
+import java.util.List;
+
 public class ManipulatorToClientPacket extends Packet {
 
     @TagField("movement")
@@ -17,7 +20,7 @@ public class ManipulatorToClientPacket extends Packet {
     @TagField("player")
     private Player player;
     @TagField("blockPos")
-    private Vec3i blockPos;
+    private List<Vec3i> blockPos;
     @TagField("gui")
     private boolean gui;
     @TagField("rotation")
@@ -27,7 +30,7 @@ public class ManipulatorToClientPacket extends Packet {
 
     }
 
-    public ManipulatorToClientPacket(Vec3d mainPos, Vec3d movement, Player player, Vec3i blockPos) {
+    public ManipulatorToClientPacket(Vec3d mainPos, Vec3d movement, Player player, List<Vec3i> blockPos) {
         this.mainPos = mainPos;
         this.movement = movement;
         this.player = player;
@@ -37,7 +40,7 @@ public class ManipulatorToClientPacket extends Packet {
 
     public ManipulatorToClientPacket(Vec3d offset, int rotation, Vec3i blockPos) {
         this.movement = offset;
-        this.blockPos = blockPos;
+        this.blockPos = Collections.singletonList(blockPos);
         this.rotation = rotation;
         gui = true;
     }
@@ -46,17 +49,21 @@ public class ManipulatorToClientPacket extends Packet {
     protected void handle() {
         if (!gui) {
             player.setPosition(mainPos);
-            BlockEntity block = getWorld().getBlockEntity(blockPos, BlockEntity.class);
-            if (block instanceof IManipulate) {
-                IManipulate manipulate = (IManipulate) block;
-                manipulate.setOffset(manipulate.getOffset().add(movement));
+            for (Vec3i bp : blockPos) {
+                BlockEntity block = getWorld().getBlockEntity(bp, BlockEntity.class);
+                if (block instanceof IManipulate) {
+                    IManipulate manipulate = (IManipulate) block;
+                    manipulate.setOffset(manipulate.getOffset().add(movement));
+                }
             }
         } else {
-            BlockEntity block = getWorld().getBlockEntity(blockPos, BlockEntity.class);
-            if (block instanceof IManipulate) {
-                IManipulate manipulate = (IManipulate) block;
-                manipulate.setOffset(movement);
-                manipulate.setRotation(rotation);
+            for (Vec3i bp : blockPos) {
+                BlockEntity block = getWorld().getBlockEntity(bp, BlockEntity.class);
+                if (block instanceof IManipulate) {
+                    IManipulate manipulate = (IManipulate) block;
+                    manipulate.setOffset(movement);
+                    manipulate.setRotation(rotation);
+                }
             }
         }
     }
