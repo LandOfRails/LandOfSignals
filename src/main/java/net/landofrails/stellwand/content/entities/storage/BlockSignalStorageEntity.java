d763cd13305c5a406752251acc3a272d522177a9
Danielxs01 <daniel.schmidtdsi@gmail.com>
2021-03-04 18:06:03 +0100

    Saving Lists with TagField works now

--- src/main/java/net/landofrails/stellwand/content/entities/storage/BlockSignalStorageEntity.java
+++ src/main/java/net/landofrails/stellwand/content/entities/storage/BlockSignalStorageEntity.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import cam72cam.mod.ModCore;
+import cam72cam.mod.serialization.SerializationException;
+import cam72cam.mod.serialization.TagCompound;
 import cam72cam.mod.serialization.TagField;
 import net.landofrails.stellwand.content.entities.function.BlockSignalFunctionEntity;
 import net.landofrails.stellwand.content.entities.rendering.BlockSignalRenderEntity;
@@ -21,6 +24,13 @@ public class BlockSignalStorageEntity extends BlockSignalFunctionEntity {
 	public float rot = 0;
 	@TagField(value = "senders", typeHint = UUID.class, mapper = UUIDListMapper.class)
 	public List<UUID> senders = new ArrayList<>();
+
+	@Override
+	public void load(TagCompound nbt) throws SerializationException {
+		ModCore.Mod.warn("Loading nbt..");
+		ModCore.Mod.warn(nbt.toString());
+		super.load(nbt);
+	}
 
 	// Subclasses
 	public BlockSignalRenderEntity renderEntity;
