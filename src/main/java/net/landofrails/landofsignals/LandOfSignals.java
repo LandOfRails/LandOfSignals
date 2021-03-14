d695e4f520b7c7b65a93a2af9df4d11813eff639
MarkenJaden <jjsch1410@gmail.com>
2021-03-15 00:30:22 +0100

    Changed version to 0.0.3

--- src/main/java/net/landofrails/landofsignals/LandOfSignals.java
+++ src/main/java/net/landofrails/landofsignals/LandOfSignals.java
@@ -21,7 +21,7 @@ import net.landofrails.landofsignals.utils.contentpacks.ContentPackHandler;
 public class LandOfSignals extends ModCore.Mod {
     @SuppressWarnings("java:S1845")
     public static final String MODID = "landofsignals";
-    public static final String VERSION = "0.0.2";
+    public static final String VERSION = "0.0.3";
 
     @Override
     public String modID() {
