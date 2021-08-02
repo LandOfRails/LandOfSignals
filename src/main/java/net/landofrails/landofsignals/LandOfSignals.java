0fb7f864a345d9d1cb074ba49efdb719049265bb
Danielxs01 <daniel.schmidtdsi@gmail.com>
2021-08-02 21:40:16 +0200

    Upgrade from UMC 1.1.0 to 1.1.1 and increased version to 0.0.4

--- src/main/java/net/landofrails/landofsignals/LandOfSignals.java
+++ src/main/java/net/landofrails/landofsignals/LandOfSignals.java
@@ -17,11 +17,9 @@ import net.landofrails.landofsignals.render.item.ObjItemRender;
 import net.landofrails.landofsignals.tile.*;
 import net.landofrails.landofsignals.utils.contentpacks.ContentPackHandler;
 
-@SuppressWarnings("java:S112")
 public class LandOfSignals extends ModCore.Mod {
-    @SuppressWarnings("java:S1845")
     public static final String MODID = "landofsignals";
-    public static final String VERSION = "0.0.3";
+    public static final String VERSION = "0.0.4";
 
     @Override
     public String modID() {
