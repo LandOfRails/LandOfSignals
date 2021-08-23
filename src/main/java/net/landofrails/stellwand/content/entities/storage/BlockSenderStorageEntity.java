d2219c855f6cd30068264f51706d30fa4502ac82
Danielxs01 <daniel.schmidtdsi@gmail.com>
2021-08-23 19:10:35 +0200

    [#17] Fixed NullPointer exception

--- src/main/java/net/landofrails/stellwand/content/entities/storage/BlockSenderStorageEntity.java
+++ src/main/java/net/landofrails/stellwand/content/entities/storage/BlockSenderStorageEntity.java
@@ -1,12 +1,5 @@
 package net.landofrails.stellwand.content.entities.storage;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import cam72cam.mod.ModCore;
 import cam72cam.mod.math.Vec3i;
 import cam72cam.mod.model.obj.OBJModel;
@@ -22,129 +15,137 @@ import net.landofrails.stellwand.contentpacks.entries.parent.ContentPackEntryBlo
 import net.landofrails.stellwand.utils.StellwandUtils;
 import net.landofrails.stellwand.utils.mapper.Vec3iListMapper;
 
+import java.util.*;
+import java.util.Map.Entry;
+
 public class BlockSenderStorageEntity extends BlockSenderFunctionEntity {
 
-	// Statics
-	public static final String MISSING = "missing";
+    // Statics
+    public static final String MISSING = "missing";
 
-	protected static Map<String, OBJModel> models = new HashMap<>();
-	protected static Map<String, OBJRender> renderers = new HashMap<>();
-	protected static Map<String, float[]> rotations = new HashMap<>();
-	protected static Map<String, float[]> translations = new HashMap<>();
+    protected static Map<String, OBJModel> models = new HashMap<>();
+    protected static Map<String, OBJRender> renderers = new HashMap<>();
+    protected static Map<String, float[]> rotations = new HashMap<>();
+    protected static Map<String, float[]> translations = new HashMap<>();
 
-	// TagFields
-	@TagField("contentPackBlockId")
-	public String contentPackBlockId = MISSING;
+    // TagFields
+    @TagField("contentPackBlockId")
+    public String contentPackBlockId = MISSING;
 
-	@TagField("blockRotation")
-	public float blockRotation = 0;
+    @TagField("blockRotation")
+    public float blockRotation = 0;
 
-	@TagField(value = "signals", typeHint = Vec3i.class, mapper = Vec3iListMapper.class)
-	public List<Vec3i> signals = new ArrayList<>();
+    @TagField(value = "signals", typeHint = Vec3i.class, mapper = Vec3iListMapper.class)
+    public List<Vec3i> signals = new ArrayList<>();
 
-	@TagField("modePowerOff")
-	public String modePowerOff;
+    @TagField("modePowerOff")
+    public String modePowerOff;
 
-	@TagField("modePowerOn")
-	public String modePowerOn;
+    @TagField("modePowerOn")
+    public String modePowerOn;
 
-	@TagField("hasPower")
-	public boolean hasPower = false;
+    @TagField("hasPower")
+    public boolean hasPower = false;
 
-	// Variables
-	private BlockSignalStorageEntity signalEntity;
+    // Variables
+    private BlockSignalStorageEntity signalEntity;
 
-	// Subclasses
-	public BlockSenderRenderEntity renderEntity;
+    // Subclasses
+    public BlockSenderRenderEntity renderEntity;
 
-	public BlockSenderStorageEntity() {
-		renderEntity = new BlockSenderRenderEntity(this);
-	}
+    public BlockSenderStorageEntity() {
+        renderEntity = new BlockSenderRenderEntity(this);
+    }
 
-	public static void prepare() {
+    public static void prepare() {
 
-		Stellwand.debug("Preparing Sender..");
+        Stellwand.debug("Preparing Sender..");
 
-		try {
-			Identifier id = new Identifier(Stellwand.DOMAIN, "models/block/others/blocknotfound/blocknotfound.obj");
-			OBJModel m = new OBJModel(id, 0);
-			models.put(MISSING, m);
-			rotations.put(MISSING, new float[]{0, 0, 0});
-			translations.put(MISSING, new float[]{0.5f, 0.5f, 0.5f});
-		} catch (Exception e) {
-			ModCore.Mod.error("Error while loading blocknotfound.obj: %s", e.getMessage());
-		}
-		// Add contentpack stuff
-		for (Entry<ContentPackEntry, String> entry : Content.getBlockSenders().entrySet()) {
-			String name = null;
-			String type = null;
-			ContentPackEntryBlock cpeb = null;
-			try {
-				ContentPackEntry cpe = entry.getKey();
-				String packId = entry.getValue();
-				String blockId = cpe.getBlockId(packId);
-				cpeb = entry.getKey().getBlock();
-				name = blockId;
-				type = cpe.getType() != null ? cpe.getType().name() : "null";
-				ContentPackEntryBlock block = cpe.getBlock();
-				String objPath = cpe.getModel();
-				Identifier id = new Identifier("stellwand", objPath);
-				OBJModel m = new OBJModel(id, 0);
-				models.put(blockId, m);
-				rotations.put(blockId, block.getRotation());
-				translations.put(blockId, block.getTranslation());
-			} catch (Exception e) {
-				ModCore.Mod.error("Error while loading contentpack blocks:");
-				ModCore.Mod.error("Block: %s", name);
-				ModCore.Mod.error("Type: %s", type);
-				StellwandUtils.printSuperclasses(cpeb);
-				e.printStackTrace();
-			}
-		}
+        try {
+            Identifier id = new Identifier(Stellwand.DOMAIN, "models/block/others/blocknotfound/blocknotfound.obj");
+            OBJModel m = new OBJModel(id, 0);
+            models.put(MISSING, m);
+            rotations.put(MISSING, new float[]{0, 0, 0});
+            translations.put(MISSING, new float[]{0.5f, 0.5f, 0.5f});
+        } catch (Exception e) {
+            ModCore.Mod.error("Error while loading blocknotfound.obj: %s", e.getMessage());
+        }
+        // Add contentpack stuff
+        for (Entry<ContentPackEntry, String> entry : Content.getBlockSenders().entrySet()) {
+            String name = null;
+            String type = null;
+            ContentPackEntryBlock cpeb = null;
+            try {
+                ContentPackEntry cpe = entry.getKey();
+                String packId = entry.getValue();
+                String blockId = cpe.getBlockId(packId);
+                cpeb = entry.getKey().getBlock();
+                name = blockId;
+                type = cpe.getType() != null ? cpe.getType().name() : "null";
+                ContentPackEntryBlock block = cpe.getBlock();
+                String objPath = cpe.getModel();
+                Identifier id = new Identifier("stellwand", objPath);
+                OBJModel m = new OBJModel(id, 0);
+                models.put(blockId, m);
+                rotations.put(blockId, block.getRotation());
+                translations.put(blockId, block.getTranslation());
+            } catch (Exception e) {
+                ModCore.Mod.error("Error while loading contentpack blocks:");
+                ModCore.Mod.error("Block: %s", name);
+                ModCore.Mod.error("Type: %s", type);
+                StellwandUtils.printSuperclasses(cpeb);
+                e.printStackTrace();
+            }
+        }
 
-	}
+    }
 
-	// Getters
-	public String getContentPackBlockId() {
-		return contentPackBlockId;
-	}
+    // Getters
+    public String getContentPackBlockId() {
+        return contentPackBlockId;
+    }
 
-	public void setContentBlockId(String contentPackBlockId) {
-		this.contentPackBlockId = contentPackBlockId;
-	}
+    public void setContentBlockId(String contentPackBlockId) {
+        this.contentPackBlockId = contentPackBlockId;
+    }
 
-	public boolean isCompatible(BlockSignalStorageEntity otherSignal) {
-		if (signals.isEmpty())
-			return true;
+    public boolean isCompatible(BlockSignalStorageEntity otherSignal) {
+        if (signals.isEmpty())
+            return true;
+        
+        Iterator<Vec3i> signal = signals.iterator();
+        BlockSignalStorageEntity signalTile;
+        do {
+            signalTile = getWorld().getBlockEntity(signal.next(), BlockSignalStorageEntity.class);
+        } while (signalTile == null && signal.hasNext());
+        if (signalTile == null)
+            return true;
+        return signalTile.getContentPackBlockId().equals(otherSignal.getContentPackBlockId());
+    }
 
-		Iterator<Vec3i> signal = signals.iterator();
-		BlockSignalStorageEntity signalTile = getWorld().getBlockEntity(signal.next(), BlockSignalStorageEntity.class);
-		return signalTile.getContentPackBlockId().equals(otherSignal.getContentPackBlockId());
-	}
+    public void setSignal(BlockSignalStorageEntity signalEntity) {
+        this.signalEntity = signalEntity;
+    }
 
-	public void setSignal(BlockSignalStorageEntity signalEntity) {
-		this.signalEntity = signalEntity;
-	}
+    public BlockSignalStorageEntity getSignal() {
+        return this.signalEntity;
+    }
 
-	public BlockSignalStorageEntity getSignal() {
-		return this.signalEntity;
-	}
+    // Getters
+    public static Map<String, OBJModel> getModels() {
+        return models;
+    }
 
-	// Getters
-	public static Map<String, OBJModel> getModels() {
-		return models;
-	}
+    public static Map<String, OBJRender> getRenderers() {
+        return renderers;
+    }
 
-	public static Map<String, OBJRender> getRenderers() {
-		return renderers;
-	}
+    public static Map<String, float[]> getRotations() {
+        return rotations;
+    }
 
-	public static Map<String, float[]> getRotations() {
-		return rotations;
-	}
-
-	public static Map<String, float[]> getTranslations() {
-		return translations;
-	}
+    public static Map<String, float[]> getTranslations() {
+        return translations;
+    }
 
 }
