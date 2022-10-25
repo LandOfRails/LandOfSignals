640ca8c83efac18bf86b978a6c0b200814de5574
Danielxs01 <daniel.schmidtdsi@gmail.com>
2022-10-25 20:41:36 +0200

    [Unification] Version 1.0.0

--- src/main/java/net/landofrails/landofsignals/LandOfSignals.java
+++ src/main/java/net/landofrails/landofsignals/LandOfSignals.java
@@ -27,7 +27,7 @@ import java.util.Optional;
 public class LandOfSignals extends ModCore.Mod {
     @SuppressWarnings({"java:S1845"})
     public static final String MODID = "landofsignals";
-    public static final String VERSION = "0.0.6";
+    public static final String VERSION = "1.0.0";
 
     @Override
     public String modID() {
--- src/main/java/net/landofrails/landofsignals/Mod.java
+++ src/main/java/net/landofrails/landofsignals/Mod.java
@@ -2,7 +2,7 @@ package net.landofrails.landofsignals;
 
 import cam72cam.mod.ModCore;
 
-@net.minecraftforge.fml.common.Mod(modid = Mod.MODID, name = "LandOfSignals", version = "0.0.4", dependencies = "required-after:universalmodcore@[1.1,1.2)", acceptedMinecraftVersions = "[1.12,1.13)")
+@net.minecraftforge.fml.common.Mod(modid = Mod.MODID, name = "LandOfSignals", version = "1.0.0", dependencies = "required-after:universalmodcore@[1.1,1.2)", acceptedMinecraftVersions = "[1.12,1.13)")
 @SuppressWarnings({"java:S112", "java:S1118"})
 public class Mod {
     public static final String MODID = "landofsignals";
