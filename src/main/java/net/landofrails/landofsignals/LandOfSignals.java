21e0f0a9ad33011265dd08b3c46adcb13202cda3
Jan Jaden Schmidt <jjsch1410@gmail.com>
2023-01-24 22:18:22 +0100

    Update LandOfSignals.java

--- src/main/java/net/landofrails/landofsignals/LandOfSignals.java
+++ src/main/java/net/landofrails/landofsignals/LandOfSignals.java
@@ -28,7 +28,7 @@ public class LandOfSignals extends ModCore.Mod {
     @SuppressWarnings({"java:S1845"})
     public static final String MODID = "landofsignals";
     // Current version
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "0.0.0";
 
     @Override
     public String modID() {
