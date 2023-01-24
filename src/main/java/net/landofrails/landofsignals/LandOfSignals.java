04c1bc6414265299a50ce67063fe7cb1f1ed6139
Danielxs01 <daniel.schmidtdsi@gmail.com>
2023-01-24 22:15:11 +0100

    [1.0.0] build.gradle version 0.0.4 -> 1.0.0

--- src/main/java/net/landofrails/landofsignals/LandOfSignals.java
+++ src/main/java/net/landofrails/landofsignals/LandOfSignals.java
@@ -27,6 +27,7 @@ import java.util.Optional;
 public class LandOfSignals extends ModCore.Mod {
     @SuppressWarnings({"java:S1845"})
     public static final String MODID = "landofsignals";
+    // Current version
     public static final String VERSION = "1.0.0";
 
     @Override
