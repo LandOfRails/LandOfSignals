2941f05d19df2be6f7c0d69684e4bd07a67bca10
MarkenJaden <jjsch1410@gmail.com>
2023-05-15 14:11:43 +0000

    Updated mod version

--- src/main/java/net/landofrails/landofsignals/LandOfSignals.java
+++ src/main/java/net/landofrails/landofsignals/LandOfSignals.java
@@ -28,7 +28,7 @@ public class LandOfSignals extends ModCore.Mod {
     @SuppressWarnings({"java:S1845"})
     public static final String MODID = "landofsignals";
     // Current version
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
 
     @Override
     public String modID() {
