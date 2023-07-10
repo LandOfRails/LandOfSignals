761cd1e5c0948bbcd8feaa4bdb7f624eb62d2a80
Daniel Schmidt <60752432+Danielxs01@users.noreply.github.com>
2023-07-10 19:27:38 +0200

    Issue-112 version set to 1.1.0
    issue-112 version set to 1.1.0

--- src/main/java/net/landofrails/landofsignals/LandOfSignals.java
+++ src/main/java/net/landofrails/landofsignals/LandOfSignals.java
@@ -29,7 +29,7 @@ public class LandOfSignals extends ModCore.Mod {
     public static final String MODID = "landofsignals";
     // Current version
     @SuppressWarnings("unused")
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.1.0";
 
     @Override
     public String modID() {
